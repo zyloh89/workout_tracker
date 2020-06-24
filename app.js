690ce3b45fe303833ccefbf31e11ea03c1b00a2b
Ziyen <zyloh89@gmail.com>
2020-06-24 22:09:42 +0930

    update route

--- app.js
+++ app.js
@@ -17,7 +17,20 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html")
+})
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+
+app.get("/api/workouts", (req, res) => {
+  res.send(200);
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
